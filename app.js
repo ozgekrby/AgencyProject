64b25988b2a8e27de37c41ca510e1ac928bac51c
ozge <karabayozge@gmail.com>
2024-12-13 18:15:35 +0300

    mongoose connection

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 const express = require("express");
 const app = express();
+const mongoose=require("mongoose");
 
 //TEMPLATE ENGINE
 app.set("view engine","ejs")
 //Middlewares
 app.use(express.static("public"))
+
+// Connect DB
+const connectWithRetry = async () => {
+  try {
+    await mongoose.connect('mongodb://localhost:27017/agency_database');
+    console.log("Connected to DB");
+  } catch (err) {
+    console.error("Database connection error:", err);
+    console.log("Retrying connection in 5 seconds...");
+    setTimeout(connectWithRetry, 5000); 
+  }
+};
+
+connectWithRetry();
 //Routes
 app.get("/", (req, res) => {
     res.status(200).render("index",{
